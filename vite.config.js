bedf2657e3e363fc341bd8a0d06f51a633ae4c53
Md.Robiul Hossain <freelance1923@gmail.com>
2025-08-21 04:47:09 +0600

    some edit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/B10A7-Assignment-7/',
   plugins: [react(),
     tailwindcss(),
   ],
